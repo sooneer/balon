1286218fe320c26814fdf0496e10b1e9c4310eec
soner.acar <soner.acar@vbt.com.tr>
2022-04-07 23:32:29 +0300

    First Commit.

---
+++ main.js
@@ -0,0 +1,20 @@
+(function() {
+    console.log('main');
+
+    var audio = new Audio('/assets/balon.wav');
+    var balonSayisi = 25;
+    SNR.Event('.balon', 'click', function() {
+        console.log(this, 'balon patladı');
+        audio.play();
+        this.classList.add("patladi");
+
+        var lstBa = SNR.Element('.patladi');
+        if (lstBa.length == balonSayisi) {
+            for (let i = 0; i < lstBa.length; i++) {
+                lstBa[i].classList.remove("patladi");
+            }
+        }
+    });
+
+
+})();
